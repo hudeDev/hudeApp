14224b36ea59407e0998a46c2c4a49ef15dfa31b
Björn-Hendrik von der Linde <bjoern-hendrik.von.der.linde@exhumedo.de>
2013-07-08 17:15:54 +0200

    filesystem

--- js/filesystem.js
+++ js/filesystem.js
@@ -4,10 +4,10 @@
  * Time: 11:56 AM
  * To change this template use File | Settings | File Templates.
  */
-var FileClient = (function () {
- 
+var FileClient = (function() {
+
     var fileSystem = null;
- 
+
     /*
      init function must be called once before using any other function of this class
      */
@@ -18,203 +18,225 @@ var FileClient = (function () {
          LocalFileSystem.TEMPORARY: Used for storage with no guarantee of persistence.
          */
         window.requestFileSystem(LocalFileSystem.PERSISTENT, 0,
-            function (result) {
-                fileSystem = result;
-                console.log('fileSystem Created at: ' + fileSystem.root.fullPath);
-                // we may call successCallback here, or after creating directories
-                createDirectory(successCallback,
-                    function (err) {
-                        if (errorCallback) errorCallback({'obj':err, 'msg':getFileErrorMessage(err)});
-                    }
-                );
-            },
-            function (err) {
-                if (errorCallback) errorCallback({'obj':err, 'msg':getFileErrorMessage(err)});
-            }
+                function(result) {
+                    fileSystem = result;
+                    console.log('fileSystem Created at: ' + fileSystem.root.fullPath);
+                    return 'fileSystem Created at: ' + fileSystem.root.fullPath;
+                    // we may call successCallback here, or after creating directories
+                    createDirectory(successCallback,
+                            function(err) {
+                                if (errorCallback)
+                                    errorCallback({'obj': err, 'msg': getFileErrorMessage(err)});
+                            }
+                    );
+                },
+                function(err) {
+                    if (errorCallback)
+                        errorCallback({'obj': err, 'msg': getFileErrorMessage(err)});
+                }
         );
     }
- 
+
     function createDirectory(successCallback, errorCallback) {
         console.log("||FileClient.createDirectory||");
- 
+
         var directoryList = ['folder1/', 'folder2/', 'folder3/'];
         var len = directoryList.length;
- 
+
         (function createNext(index) {
             if (index < len) {
-                fileSystem.root.getDirectory(directoryList [index], {create:true, exclusive:false},
-                    function (parent) {
-                        console.log("directory: " + directoryList [index] + " created successfully");
-                        createNext(index + 1);
-                    },
-                    function (err) {
-                        console.log("directory: " + directoryList [index] + " failed to create");
-                        if (errorCallback) errorCallback({'obj':err, 'msg':getFileErrorMessage(err)});
-                    }
+                fileSystem.root.getDirectory(directoryList [index], {create: true, exclusive: false},
+                function(parent) {
+                    console.log("directory: " + directoryList [index] + " created successfully");
+                    createNext(index + 1);
+                },
+                        function(err) {
+                            console.log("directory: " + directoryList [index] + " failed to create");
+                            if (errorCallback)
+                                errorCallback({'obj': err, 'msg': getFileErrorMessage(err)});
+                        }
                 );
             } else {
                 successCallback();
             }
         })(0);      // calling the createNext function with arg 0
     }
- 
+
     function writeToFile(file, data, successCallback, errorCallback) {
         console.log("||FileClient.writeToFile||");
-        fileSystem.root.getFile(file, {create:true, exclusive:false},
-            function (fileEntry) {
-                fileEntry.createWriter(
-                    function (writer) {
+        fileSystem.root.getFile(file, {create: true, exclusive: false},
+        function(fileEntry) {
+            fileEntry.createWriter(
+                    function(writer) {
                         writer.write(data);
-                        writer.onwrite = function (evt) {  // Called when the request has completed successfully.
-                            if (successCallback) successCallback(evt);
+                        writer.onwrite = function(evt) {  // Called when the request has completed successfully.
+                            if (successCallback)
+                                successCallback(evt);
                         }
                     },
-                    function (err) {
-                        if (errorCallback) errorCallback({'obj':err, 'msg':getFileErrorMessage(err)});
+                    function(err) {
+                        if (errorCallback)
+                            errorCallback({'obj': err, 'msg': getFileErrorMessage(err)});
                     }
-                );
-            },
-            function (err) {
-                if (errorCallback) errorCallback({'obj':err, 'msg':getFileErrorMessage(err)});
-            }
+            );
+        },
+                function(err) {
+                    if (errorCallback)
+                        errorCallback({'obj': err, 'msg': getFileErrorMessage(err)});
+                }
         );
     }
- 
+
     function readFileContent(filepath, successCallback, errorCallback) {
         console.log("||FileClient.readFileContent||");
-        fileSystem.root.getFile(filepath, {create:true},
-            function (fileEntry) {
-                fileEntry.file(
-                    function (file) {
+        fileSystem.root.getFile(filepath, {create: true},
+        function(fileEntry) {
+            fileEntry.file(
+                    function(file) {
                         var reader = new FileReader();
-                        reader.onload = function (evt) {  // Called when the read has successfully completed.
-                            if (successCallback) successCallback(evt.target.result);
+                        reader.onload = function(evt) {  // Called when the read has successfully completed.
+                            if (successCallback)
+                                successCallback(evt.target.result);
                         };
-                        reader.onerror = function (err) {
-                            if (errorCallback) errorCallback({'obj':err, 'msg':getFileErrorMessage(err)});
+                        reader.onerror = function(err) {
+                            if (errorCallback)
+                                errorCallback({'obj': err, 'msg': getFileErrorMessage(err)});
                         };
                         reader.readAsText(file);
                     },
-                    function (err) {
-                        if (errorCallback) errorCallback({'obj':err, 'msg':getFileErrorMessage(err)});
+                    function(err) {
+                        if (errorCallback)
+                            errorCallback({'obj': err, 'msg': getFileErrorMessage(err)});
                     }
-                );
-            },
-            function (err) {
-                if (errorCallback) errorCallback({'obj':err, 'msg':getFileErrorMessage(err)});
-            }
+            );
+        },
+                function(err) {
+                    if (errorCallback)
+                        errorCallback({'obj': err, 'msg': getFileErrorMessage(err)});
+                }
         );
     }
- 
+
     function removeFile(file, successCallback, errorCallback) {
         console.log("||FileClient.removeFile||");
-        fileSystem.root.getFile(file, {create:false, exclusive:true},
-            function (fileEntry) {
-                fileEntry.remove(successCallback,
-                    function (err) {
-                        if (errorCallback) errorCallback({'obj':err, 'msg':getFileErrorMessage(err)});
+        fileSystem.root.getFile(file, {create: false, exclusive: true},
+        function(fileEntry) {
+            fileEntry.remove(successCallback,
+                    function(err) {
+                        if (errorCallback)
+                            errorCallback({'obj': err, 'msg': getFileErrorMessage(err)});
                     }
-                );
-            },
-            function (err) {
-                if (err.code == FileError.NOT_FOUND_ERR) {
-                    // file not found means its not there or may be already deleted. calling successHandler ...
-                    if (successCallback) successCallback();
-                } else {
-                    if (errorCallback) errorCallback({'obj':err, 'msg':getFileErrorMessage(err)});
+            );
+        },
+                function(err) {
+                    if (err.code == FileError.NOT_FOUND_ERR) {
+                        // file not found means its not there or may be already deleted. calling successHandler ...
+                        if (successCallback)
+                            successCallback();
+                    } else {
+                        if (errorCallback)
+                            errorCallback({'obj': err, 'msg': getFileErrorMessage(err)});
+                    }
                 }
-            }
         );
     }
- 
+
     function removeDirectory(directoryPath, successCallback, errorCallback) {
         console.log("||FileClient.removeFile||");
         fileSystem.root.getDirectory(directoryPath, {},
-            function (parent) {
-                parent.removeRecursively(successCallback, errorCallback);
-            },
-            function (err) {
-                if (errorCallback) errorCallback({'obj':err, 'msg':getFileErrorMessage(err)});
- 
-            });
+                function(parent) {
+                    parent.removeRecursively(successCallback, errorCallback);
+                },
+                function(err) {
+                    if (errorCallback)
+                        errorCallback({'obj': err, 'msg': getFileErrorMessage(err)});
+
+                });
     }
- 
+
     function removeFilesInADirectory(directoryPath, successCallback, errorCallback) {
         console.log("||FileClient.removeFile||");
         fileSystem.root.getDirectory(directoryPath, {},
-            function (parent) {                                 // successHandler: directory found
-                var dirReader = parent.createReader();          // creating a reader to read the directory
-                dirReader.readEntries(// trying to read the directory
-                    function (entries) {
-                        var len = entries.length;               // directory files list
- 
-                        (function deleteNext(currIndex) {
-                            currIndex++;
-                            if (currIndex < len) {
-                                entries [currIndex].remove(// removing a single file
-                                    function (data) {
-                                        deleteNext(currIndex + 1);
-                                    },
-                                    function (err) {
-                                        // there is an error though, shouldn't we try to continue?
-                                        deleteNext(currIndex + 1);
+                function(parent) {                                 // successHandler: directory found
+                    var dirReader = parent.createReader();          // creating a reader to read the directory
+                    dirReader.readEntries(// trying to read the directory
+                            function(entries) {
+                                var len = entries.length;               // directory files list
+
+                                (function deleteNext(currIndex) {
+                                    currIndex++;
+                                    if (currIndex < len) {
+                                        entries [currIndex].remove(// removing a single file
+                                                function(data) {
+                                                    deleteNext(currIndex + 1);
+                                                },
+                                                function(err) {
+                                                    // there is an error though, shouldn't we try to continue?
+                                                    deleteNext(currIndex + 1);
+                                                }
+                                        );
+                                    } else {
+                                        if (successCallback)
+                                            successCallback();
                                     }
-                                );
-                            } else {
-                                if (successCallback) successCallback();
+                                })(0);
+                            },
+                            function(err) {
+                                if (errorCallback)
+                                    errorCallback({'obj': err, 'msg': getFileErrorMessage(err)});
                             }
-                        })(0);
-                    },
-                    function (err) {
-                        if (errorCallback) errorCallback({'obj':err, 'msg':getFileErrorMessage(err)});
-                    }
-                );
-            },
-            function (err) {
-                if (errorCallback) errorCallback({'obj':err, 'msg':getFileErrorMessage(err)});
-            }
+                    );
+                },
+                function(err) {
+                    if (errorCallback)
+                        errorCallback({'obj': err, 'msg': getFileErrorMessage(err)});
+                }
         );
     }
- 
+
     function fileUpload(filePath, successCallback, errorCallback) {
         console.log("||FileClient.fileUpload||");
         var options = new FileUploadOptions();
         options.fileKey = "file";
         options.fileName = filePath.substr(filePath.lastIndexOf('/') + 1);
         options.mimeType = "text/plain";
-        options.params = {'key1':'value1', 'key2':'value2'};  // additional data, json object
- 
+        options.params = {'key1': 'value1', 'key2': 'value2'};  // additional data, json object
+
         var fileTransfer = new FileTransfer();
         fileTransfer.upload(filePath, encodeURI("https://file.server.com/upload"),
-            function (data) {
-                console.log("Response: " + data.response);
-                if (successCallback) successCallback(data);
-            },
-            function (err) {
-                if (errorCallback) errorCallback({'obj':err, 'msg':getFileTransferErrorMessage(err)});
-            }
-            , options);
+                function(data) {
+                    console.log("Response: " + data.response);
+                    if (successCallback)
+                        successCallback(data);
+                },
+                function(err) {
+                    if (errorCallback)
+                        errorCallback({'obj': err, 'msg': getFileTransferErrorMessage(err)});
+                }
+        , options);
     }
- 
+
     function fileDownload(remoteUrl, localPath, successHandler, errorHandler) {
         console.log("||FileClient.fileUpload||");
         var fileTransfer = new FileTransfer();
         fileTransfer.download(remoteUrl, localPath,
-            function (data) {
-                console.log("download complete: " + data.fullPath);
-                if (successHandler) successHandler(data);
-            },
-            function (err) {
-                if (errorCallback) errorCallback({'obj':err, 'msg':getFileTransferErrorMessage(err)});
-            }
+                function(data) {
+                    console.log("download complete: " + data.fullPath);
+                    if (successHandler)
+                        successHandler(data);
+                },
+                function(err) {
+                    if (errorCallback)
+                        errorCallback({'obj': err, 'msg': getFileTransferErrorMessage(err)});
+                }
         );
- 
+
     }
- 
+
     function getFileErrorMessage(err) {
         console.log("||FileClient.getFileErrorMessage||");
         var ret = "";
- 
+
         switch (err.code) {
             case FileError.NOT_FOUND_ERR:
                 ret = "Not Found Error";
@@ -258,16 +280,16 @@ var FileClient = (function () {
             default:
                 ret = "Undefined Error";
         }
- 
+
         ret = "Err code: " + err.code + " Err Msg: " + ret;
- 
+
         return ret;
     }
- 
+
     function getFileTransferErrorMessage(err) {
         console.log("||FileClient.getFileTransferErrorMessage||");
         var ret = "";
- 
+
         switch (err.code) {
             case FileTransferError.FILE_NOT_FOUND_ERR:
                 ret = "File Not Found Error";
@@ -284,18 +306,23 @@ var FileClient = (function () {
             default:
                 ret = "Undefined Error";
         }
- 
+
         ret = "Err code: " + err.code + " Source: " + err.source + " Target: " + err.target + " HttpStatus: " + err.http_status + " Err Msg: " + ret;
- 
+
         return ret;
     }
- 
+
     var pub = {};
- 
+
     pub.init = init;
     pub.createDirectory = createDirectory;
     pub.writeToFile = writeToFile;
- 
+
     return pub;
- 
+
 })();
+
+function testerKlasse() {
+    fc = new FileClient();
+    alert(print_r(fc.init()));
+}
